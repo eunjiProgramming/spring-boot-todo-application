457bd76e8d5d922f789fc5edec82d0897861fecf
eunjiProgramming <estelle.programming@gmail.com>
2023-10-30 18:04:01 +0900

    Displaying database records to view

--- src/main/java/com/estelle/springboottodoapplication/controllers/TodoItemController.java
+++ src/main/java/com/estelle/springboottodoapplication/controllers/TodoItemController.java
@@ -1,19 +1,29 @@
 package com.estelle.springboottodoapplication.controllers;
 
+import com.estelle.springboottodoapplication.repositories.TodoItemRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.time.Instant;
+import java.time.ZoneId;
 
 @Controller
 public class TodoItemController {
     private final Logger logger = LoggerFactory.getLogger(TodoItemController.class);
 
+    @Autowired
+    private TodoItemRepository todoItemRepository;
+
     @GetMapping("/")
     public ModelAndView index() {
         logger.info("request to GET index");
         ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("todoItems", todoItemRepository.findAll());
+        modelAndView.addObject("today", Instant.now().atZone(ZoneId.systemDefault()).toLocalDate().getDayOfWeek());
         return modelAndView;
     }
 }
